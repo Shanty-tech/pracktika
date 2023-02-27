1e0efce62a25722907f2ecc02eb7c6b875cb5e5b
Your Name <you@example.com>
2023-02-27 04:32:46 -0800

    start

---
+++ storage/framework/views/8341c19f5602d894f9907e17da3de2ae9c54c536.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../resources/css/style.css">
+    <title>True Gamers</title>
+</head>
+<body>
+        
+    <div class="menu">
+        <a href="">О нас</a>
+        <a href="">Каталог</a>
+        <a href="">Где нас найти</a>
+        <a href="register">Регистрация</a>
+        <a href="login">Вход</a>
+    </div>
+
+    <div class="title">
+        <p>Мы продаем исключительно исклюзивные игры! <br> У нас вы найдете игры по вкусу и цвету, мы на рынке уже более 3-х лет <br>
+    Регистрируйся на сайте и получи скидку 25% на все товары!</p>
+    </div>
+
+    <div class="new_tovar">
+        <p>Ниже представлены новинки нашего сайта!</p>
+    </div>
+<div class="carusel">
+    <div class="itms1" id="prev"><</div>
+        <div class="card">
+            <div id="imgage_c">
+                <img src="../resources/img/13014.jpg" alt="">
+            </div>
+            <div id="imgage_c">
+                <img src="../resources/img/cd79fb5a-77da-4bd8-a41f-55bd5c8dcca8.jpg" alt="">
+            </div>
+            <div id="imgage_c">
+                <img src="../resources/img/df8fbc9126d8c6d4_1920xH.jpg" alt="">
+            </div>
+            <div id="imgage_c">
+                <img src="../resources/img/6068989288.jpg" alt="">
+            </div>
+            <div id="imgage_c">
+                <img src="../resources/img/vneshnii-zhestkii-disk-seagate-sthb2000401-0-small.jpg" alt="">
+            </div>
+        </div>
+    <div class="items2" id="next">></div>
+    
+</div>
+
+<div class="footer">
+    <div class="down_menu">
+    <a href="">О нас</a>
+        <a href="">Каталог</a>
+        <a href="">Где нас найти</a>
+        <a href="register">Регистрация</a>
+        <a href="login">Вход</a>
+    </div>
+  
+</div>
+
+<script src="../resources/js/script.js"></script>
+
+
+</body>
+</html><?php /**PATH /srv/users/yaujryim/gghtcpd-m1/resources/views/welcome.blade.php ENDPATH**/ ?>
